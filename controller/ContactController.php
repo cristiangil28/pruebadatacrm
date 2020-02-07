daa775e0a194e50b1e57ca6e0d7e650b8321911e
cristian gil <cristianingeniero2018@hotmail.com>
2020-02-06 23:24:52 -0500

    refactor code

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -1,5 +1,14 @@
 <?php
-require_once('../model/Contact.php');
-$contact = new Contact();
-$datos = $contact->getContacts();
-require_once('../datos.php');
+class ContactController
+{
+    public function index()
+    {
+        require_once('../model/Contact.php');
+        $contact = new Contact();
+        $datos = $contact->getContacts();
+        require_once('../datos.php');
+    }
+}
+
+$controller = new ContactController();
+$controller->index();
